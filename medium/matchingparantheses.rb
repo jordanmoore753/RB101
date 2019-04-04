cd0fff110df8be5ab2dcf9651df135c8cedb6efa
Jordan Moore <46928924+jordanmoore753@users.noreply.github.com>
2019-04-04 13:42:35 -0400

    Create matchingparantheses.rb

---
+++ medium/matchingparantheses.rb
@@ -0,0 +1,19 @@
+# input = a string argument passed into method
+
+# output = a boolean
+
+# rules = count the total amount of characters in
+# string, then calc the lowercase, uppercase, and nonword
+# characters.
+
+# convert the totals into percentages and make them the
+# values of the hash 
+
+# ex =
+
+
+
+
+
+
+
